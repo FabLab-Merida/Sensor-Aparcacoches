2606d3afad4940c17a722a72f35054dfa61cb871
ThePromidius <thepromidiusyt@gmail.com>
2024-10-10 17:49:51 +0200

    Update SensorAparcacoches.ino

--- Code/SensorAparcacoches.ino
+++ Code/SensorAparcacoches.ino
@@ -1,114 +1,27 @@
-/*
-* CONFIGURACION DE LA DISTRIBUCION DE PINES
-*/
-
-#include <Adafruit_NeoPixel.h>
-#define pin_trig_sensor 3
-#define pin_echo_sensor 2
-
-#define pin_neo 8
-#define pin_buzzer 6
-
-/*
-* CONFIGURACION DE LAS VARIABLES
-*/
-
-#define limite_rojo 25
-#define limite_verde 40
-#define numero_pixeles 8
-/*
-* PROGRAMA
-*/
-
-int contadorVerde = 0;    //Variable utiizada para temporizar el tiempo que lleva consecutivo en estado verde
-int contadorNaranja = 0;  //Variable utiizada para temporizar el tiempo que lleva consecutivo en estado naranja
-int contadorRojo = 0;     //Variable utiizada para temporizar el tiempo que lleva consecutivo en estado rojo
-int distancia = 0;
-Adafruit_NeoPixel tira = Adafruit_NeoPixel(numero_pixeles, pin_neo, NEO_GRB + NEO_KHZ800);
-
-
+#define pin_echo_sensor 8 //Sensor ultrasonido pin 8
+#define pin_trigger_sensor 9 //Sensor ultrasonido pin 9
 void setup() {
-  Serial.begin(9600);
-
-  pinMode(pin_trig_sensor, OUTPUT);
-  pinMode(pin_echo_sensor, INPUT);
-  pinMode(pin_buzzer, OUTPUT);
-
-  tira.begin();
+ //El pin será una entrada para leer el sensor de ultrasonidos
+ pinMode(pin_echo_sensor, INPUT);
+ //El pin enviará señales ultrasónicas, será una salida.
+ pinMode(pin_trigger_sensor, OUTPUT);
+ Serial.begin(9600);
 }
-
 void loop() {
-
-  // Ponemos el Triiger en estado bajo y esperamos 2 ms
-  digitalWrite(pin_trig_sensor, LOW);
-  delayMicroseconds(2);
-
-  // Ponemos el pin Trigger a estado alto y esperamos 10 ms
-  digitalWrite(pin_trig_sensor, HIGH);
-  delayMicroseconds(10);
-
-  // Comenzamos poniendo el pin Trigger en estado bajo
-  digitalWrite(pin_trig_sensor, LOW);
-
-  unsigned long tiempo = pulseIn(pin_echo_sensor, HIGH);
-  distancia = tiempo * 0.000001 * 34000 / 2.0;
-  Serial.print(distancia);
-  Serial.print("cm");
-  Serial.println();
-  delay(250);
-  if (distancia > limite_verde) {
-    //Ponemos a 0 los ocntadores de las otras distancias y contamos en la distancia verde
-    contadorVerde = contadorVerde + 1;
-    contadorNaranja = 0;
-    contadorRojo = 0;
-    //Si aun no ha contado 20 veces consecutivas activa el led y mantiene el buzzer descativado
-    if (contadorVerde < 20) {
-      tira.fill(tira.Color(0, 255, 0), 0, 8);
-      tira.show();
-      noTone(pin_buzzer);
-      delay(100);
-      //Si ya ha contado más de 20 veces consecutivas desactiva el LED
-    } else {
-      tira.fill(tira.Color(0, 0, 0), 0, 8);
-      tira.show();
-      noTone(pin_buzzer);
-      delay(100);
-    }
-  } else if (distancia < limite_verde && distancia > limite_rojo) {
-    //Ponemos a 0 los ocntadores de las otras distancias y contamos en la distancia naranja
-    contadorVerde = 0;
-    contadorNaranja = contadorNaranja + 1;
-    contadorRojo = 0;
-    //Si aun no ha contado 20 veces consecutivas activa el led y activa el buzzer a la frecuencia deseada
-    if (contadorNaranja < 20) {
-      tira.fill(tira.Color(255, 100, 0), 0, 8);
-      tira.show();
-      tone(pin_buzzer, 200, 100);
-      delay(100);
-      //Si ya ha contado más de 20 veces consecutivas desactiva el LED y el buzzer
-    } else {
-      tira.fill(tira.Color(0, 0, 0), 0, 8);
-      tira.show();
-      noTone(pin_buzzer);
-      delay(100);
-    }
-  } else if (distancia < limite_rojo) {
-    //Ponemos a 0 los ocntadores de las otras distancias y contamos en la distancia roja
-    contadorVerde = 0;
-    contadorNaranja = 0;
-    contadorRojo = contadorRojo + 1;
-    //Si aun no ha contado 20 veces consecutivas activa el led y activa el buzzer a la frecuencia deseada
-    if (contadorRojo < 20) {
-      tira.fill(tira.Color(255, 0, 0), 0, 8);
-      tira.show();
-      tone(pin_buzzer, 200);
-      delay(100);
-      //Si ya ha contado más de 20 veces consecutivas desactiva el LED y el buzzer
-    } else {
-      tira.fill(tira.Color(0, 0, 0), 0, 8);
-      tira.show();
-      noTone(pin_buzzer);
-      delay(100);
-    }
-  }
+ long distancia;
+ unsigned long tiempo;
+ // Ponemos el Triiger en estado bajo y esperamos 2 ms
+ digitalWrite(pin_trigger_sensor, LOW);
+ delayMicroseconds(2);
+ // Ponemos el pin Trigger a estado alto y esperamos 10 ms
+ digitalWrite(pin_trigger_sensor, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(pin_trigger_sensor, LOW);
+ //calculamos el tiempo que tarda en recibir la señal
+ tiempo = pulseIn(pin_echo_sensor, HIGH);
+ //Calculo de la distancia partiendo del tiempo
+ distancia = tiempo * 0.000001 * 34000 / 2.0;
+ //envío de la distancia al ordenador
+ Serial.print(distancia);
+ Serial.println(“cm”);
 }
